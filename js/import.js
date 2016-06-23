896627c7b5f42b548b67b03fe8e15d3c2919e14f
Shubham Bhadani <shubham.0794x@gmail.com>
2016-06-23 13:14:41 +0530

    Update import.js

--- js/import.js
+++ js/import.js
@@ -49,3 +49,52 @@ function discardObjectAjax()
     request.setCallbackSuccess( discardObjectCallback );
     request.send( url );
 }
+function discardObjectCallback( xmlElement )
+{
+	var type = xmlElement.getAttribute( "type" );
+    var discardedElements = xmlElement.firstChild.nodeValue;
+    
+    var elementType = document.getElementById( "type" ).value;
+	var elementStatus = document.getElementById( "status" ).value;
+	
+	if ( type == "success" )
+    {
+    	if ( elementType == "CONSTANT" && elementStatus == "NEW" )
+		{
+			handleField( discardedElements, "newConstantSpan", "newConstantTd" );
+		}
+    	else if ( elementType == "CONSTANT" && elementStatus == "UPDATE" )
+		{
+			handleField( discardedElements, "updateConstantSpan", "updateConstantTd" );
+		}
+		else if ( elementType == "DATAELEMENT" && elementStatus == "NEW" )
+		{
+			handleField( discardedElements, "newDataElementSpan", "newDataElementTd" );
+		}
+    	else if ( elementType == "DATAELEMENT" && elementStatus == "UPDATE" )
+		{
+			handleField( discardedElements, "updateDataElementSpan", "updateDataElementTd" );
+		}
+		
+function discardObjectsOfTypeCallback( xmlElement )
+{
+	var type = xmlElement.getAttribute( "type" );
+	
+	var elementType = document.getElementById( "type" ).value;
+		
+	if ( type == "success" )
+	{
+		// Set value count to none
+		
+		if ( elementType == "CONSTANT" )
+		{
+			clearField( "newConstantTd" );
+			clearField( "updateConstantTd" );
+		}
+		else if ( elementType == "DATAELEMENT" )
+		{
+			clearField( "newDataElementTd" );
+			clearField( "updateDataElementTd" );
+			clearField( "newIndicatorTd" );
+			clearField( "updateIndicatorTd" );
+		}
